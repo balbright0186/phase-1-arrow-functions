42f04a9a96493b2a72c32c754c682d397e84dd27
Bethany Albright <balbright0186@gmail.com>
2023-04-08 21:37:25 -0400

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 2000;
+let b = 100;
+let divide = (a, b) => {
+    return (a / b);
+};
+
+let square = (x) => {
+        return(x**x);
+    };
+
+let k = 3;
+let l = 4;
+let add = (k, l) => {
+    return (k + l);
+}
